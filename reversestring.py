3b224b959268f53a86d493ed01a5279187efeb33
Juan Martinez <juangamartinez_98@hotmail.com>
2018-04-21 11:28:11 -0500

    Escritura-al-reves
    Hemos generado este código que se encarga de revertir todos los datos de un string

---
+++ reversestring.py
@@ -0,0 +1,13 @@
+#datos ingresados fue la frase struggle why white stay job set keep about
+ 
+
+palabra2 ="struggle why white stay job set keep about"
+strinvert = ''
+for i in palabra2:
+	strinvert = i + strinvert
+ 
+print (strinvert)
+
+salir=input()
+
+#solucion tuoba peek tes boj yats etihw yhw elggurts
